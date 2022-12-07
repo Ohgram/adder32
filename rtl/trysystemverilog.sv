dc0fce600ff2679b93aa7981aeecde13ea3ca5b3
hLINUX <1197621972@qq.com>
2022-12-07 22:41:38 +0800

    my first systemverilog code

---
+++ rtl/trysystemverilog.sv
@@ -0,0 +1,10 @@
+program test;
+    // typedef enum {IDLE,TEST,START} state;
+    // enum bit[2:0]{S0='b001,S1='b010,S2='b100} st;
+    // state c_st,n_st=IDLE;
+    // $display( "st= %3b,n_st = %s",st,n_st.name);
+    initial begin
+    $display("hello,world");   
+    end
+    
+endprogram
